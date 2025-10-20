03ac5213b1fd467e8ec5f92f16c995ebb869d563
brayanmtzzz <zs22017044@estudiantes.uv.mx>
2025-10-19 22:46:54 -0600

    Categorías mejorado

--- src/main/java/com/example/gestor_tareas/dto/QuoteDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.gestor_tareas.dto;
-
-import lombok.Data;
-
-@Data
-public class QuoteDTO {
-    private String content;
-    private String author;
-}
--- src/main/java/com/example/gestor_tareas/service/KeepAliveService.java
+++ src/main/java/com/example/gestor_tareas/service/KeepAliveService.java
@@ -13,14 +13,13 @@ public class KeepAliveService {
     @Value("${SELF_URL:http://localhost:8080}")
     private String selfUrl;
     
-    // Se ejecuta cada 11 minutos y 40 segundos (700,000 ms)
     @Scheduled(fixedRate = 700000)
     public void keepAlive() {
         try {
             restTemplate.getForObject(selfUrl, String.class);
-            System.out.println("✅ Keep-alive ping sent to " + selfUrl);
+            System.out.println("Keep-alive ping sent to " + selfUrl);
         } catch (Exception e) {
-            System.err.println("❌ Failed to send keep-alive ping: " + e.getMessage());
+            System.err.println("Failed to send keep-alive ping: " + e.getMessage());
         }
     }
 }
